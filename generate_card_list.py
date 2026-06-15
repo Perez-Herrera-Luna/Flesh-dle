124ea30fe4a749502d796fb15b2a49000579caf9
Luna Perez-Herrera <lunaperezherrera@gmail.com>
2026-06-15 12:04:08 -0700

    Fixed issue with how cards with multiple rarities were handled

--- generate_card_list.py
+++ generate_card_list.py
@@ -33,6 +33,7 @@ for card in cards:
         # Card already exists in set but we want to append set_id
         match_index = find_match_index(card["name"], card["color"], card_list)
         card_list[match_index]["set_id"].add(card["set_id"])
+        card_list[match_index]["rarity"].add(card["rarity"])
 
         # set_id has been updates, no need to create a new entry
         continue
@@ -53,7 +54,7 @@ for card in cards:
     card_dict.update({"types": card["types"]})
     card_dict.update({"set_id": set([card["set_id"]])})
     card_dict.update({"card_keywords": card["card_keywords"]})
-    card_dict.update({"rarity": card["rarity"]})
+    card_dict.update({"rarity": set([card["rarity"]])})
 
     card_list.append(card_dict)
 
@@ -61,6 +62,7 @@ for card in cards:
 # set_id is currently a set and needs to be cast to a list for writing out as JSON
 for card in card_list:
     card.update({"set_id": list(card["set_id"])})
+    card.update({"rarity": list(card["rarity"])})
 
 # Write JSON file with cards
 with open("cards.json", mode="w", encoding="utf-8") as f:
