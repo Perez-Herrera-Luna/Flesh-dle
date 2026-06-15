9d3bb780e5b46b178400efe5ba377d3045f9f3a4
Luna Perez-Herrera <lunaperezherrera@gmail.com>
2026-06-15 12:25:53 -0700

    Refactored to support new format in update card database

--- generate_card_list.py
+++ generate_card_list.py
@@ -1,14 +1,6 @@
 import json
 import argparse
 
-
-def find_match_index(card_name: str, card_color: str, search_list: list[dict]) -> int:
-    for i, card_i in enumerate(search_list):
-        if card_name == card_i["name"] and card_color == card_i["color"]:
-            return i
-    return -1
-
-
 # Argument Parser
 parser = argparse.ArgumentParser(
     prog="GenerateCardList",
@@ -23,24 +15,8 @@ with open(args.filename, mode="r", encoding="utf-8") as read_file:
 
 
 # Form list with every card + color
-card_set = set()
 card_list = list()
 for card in cards:
-    # Check that we haven't already added the current card to the list
-    card_tuple = tuple((card["name"], card["color"]))
-
-    if card_tuple in card_set:
-        # Card already exists in set but we want to append set_id
-        match_index = find_match_index(card["name"], card["color"], card_list)
-        card_list[match_index]["set_id"].add(card["set_id"])
-        card_list[match_index]["rarity"].add(card["rarity"])
-
-        # set_id has been updates, no need to create a new entry
-        continue
-
-    # Add name & color tuple to set for duplicate identification
-    card_set.add(card_tuple)
-
     # Form the list element
     card_dict = dict()
     card_dict.update({"name": card["name"]})
@@ -52,14 +28,21 @@ for card in cards:
     card_dict.update({"health": card["health"]})
     card_dict.update({"intelligence": card["intelligence"]})
     card_dict.update({"types": card["types"]})
-    card_dict.update({"set_id": set([card["set_id"]])})
     card_dict.update({"card_keywords": card["card_keywords"]})
-    card_dict.update({"rarity": set([card["rarity"]])})
+
+    # Initialize empty sets for elements that vary with multiple printings
+    card_dict.update({"set_id": set()})
+    card_dict.update({"rarity": set()})
+
+    # Add elements that vary with multiple printings
+    for printing in card["printings"]:
+        card_dict["set_id"].add(printing["set_id"])
+        card_dict["rarity"].add(printing["rarity"])
 
     card_list.append(card_dict)
 
 
-# set_id is currently a set and needs to be cast to a list for writing out as JSON
+# Convert non-unique elements to lists for writing as JSON
 for card in card_list:
     card.update({"set_id": list(card["set_id"])})
     card.update({"rarity": list(card["rarity"])})
